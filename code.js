ca50c570584cde7b09490419012889fcc8496f56
devsajibghosh <devsajibghosh@gmail.com>
2024-09-01 13:24:15 +0600

    3 problem sloved

---
+++ code.js
@@ -0,0 +1,38 @@
+const oneKgApplePrice = 120;
+const oneKgorangePrice = 220;
+const payMoney = 1000;
+const totalFruitsPrice = 120 + 220;
+const moneyBack = payMoney - totalFruitsPrice ;
+
+// console.log(moneyBack);
+
+// and then___ 
+
+//  calculate the average marks of Mathematic
+
+const allSubjectMarks = 75.25 +  65 + 80 + 35.45 + 99.50 ;
+const averageMarks = allSubjectMarks / 5 ;
+const rounded = averageMarks.toFixed(2);
+
+// console.log(rounded);
+
+
+// given number 5 and 119 modules here 
+
+const givenNumberIs = 5;
+const userNumberIs = 119;
+const userPercentage = userNumberIs / 2;
+const modalNumberIs = userPercentage % 5;
+const floorNumberIs = Math.floor(modalNumberIs);
+console.log(floorNumberIs);
+
+
+
+
+
+
+
+
+
+
+
